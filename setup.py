de4aed155da5eb3bbb44b7fa91ba941b6436e41e
James Timmins <jameshtimmins@gmail.com>
2018-09-06 19:59:02 -0700

    Add long_description.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
+long_description = 'The official Python library for IPinfo. IPinfo prides itself on being the most reliable, accurate, and in-depth source of IP address data available anywhere. We process terabytes of data to produce our custom IP geolocation, company, carrier and IP type data sets. You can visit our developer docs at https://ipinfo.io/developers.'
+
 setup(name='ipinfo_wrapper',
-      version='0.1.5',
+      version='0.1.6',
       description='Official Python library for IPInfo',
+      long_description=long_description,
       url='https://github.com/ipinfo/python',
       author='James Timmins',
       author_email='jameshtimmins@gmail.com',
