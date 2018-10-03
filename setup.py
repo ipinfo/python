5780502c3efdf04bce17b4e7c8fca2782bb561fb
James Timmins <jameshtimmins@gmail.com>
2018-10-03 12:44:51 -0700

    Update version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 long_description = 'The official Python library for IPinfo. IPinfo prides itself on being the most reliable, accurate, and in-depth source of IP address data available anywhere. We process terabytes of data to produce our custom IP geolocation, company, carrier and IP type data sets. You can visit our developer docs at https://ipinfo.io/developers.'
 
 setup(name='ipinfo',
-      version='1.0.2',
+      version='1.0.3',
       description='Official Python library for IPInfo',
       long_description=long_description,
       url='https://github.com/ipinfo/python',
