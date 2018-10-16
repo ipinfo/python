af24f610eae3f397afd187873341fec5a556466f
Uman Shahzad <uman@opencraft.com>
2018-10-16 11:29:41 +0500

    Make long-description a multi-line string for easier editing.

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 
-long_description = 'The official Python library for IPinfo. IPinfo prides itself on being the most reliable, accurate, and in-depth source of IP address data available anywhere. We process terabytes of data to produce our custom IP geolocation, company, carrier and IP type data sets. You can visit our developer docs at https://ipinfo.io/developers.'
+long_description = """
+The official Python library for IPinfo.
+
+IPinfo prides itself on being the most reliable, accurate, and in-depth source of IP address data available anywhere.
+We process terabytes of data to produce our custom IP geolocation, company, carrier and IP type data sets.
+You can visit our developer docs at https://ipinfo.io/developers.
+"""
 
 setup(name='ipinfo',
       version='1.1.0',
