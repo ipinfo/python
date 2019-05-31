d2b611fe3e6da385a13a19a3c7c228d2688743ce
Uman Shahzad <uman@mslm.me>
2019-06-01 02:39:12 +0500

    Patch fix version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ You can visit our developer docs at https://ipinfo.io/developers.
 
 setup(
     name="ipinfo",
-    version="1.1.1",
+    version="1.1.2",
     description="Official Python library for IPInfo",
     long_description=long_description,
     url="https://github.com/ipinfo/python",
