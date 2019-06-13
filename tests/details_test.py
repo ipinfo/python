5e38c95e21201a7605209e9ae8ab2d8423723379
Uman Shahzad <uman@mslm.me>
2019-06-13 20:56:39 +0500

    Fix tests, update requirements, test for json serialization

--- tests/details_test.py
+++ tests/details_test.py
@@ -4,25 +4,25 @@ from ipinfo.details import Details
 
 
 def test_init():
-    data = {'foo': 'bar'}
+    data = {"foo": "bar"}
     details = Details(data)
     assert details.details == data
 
 
 def test_getattr_success():
-    data = {'foo': 'bar'}
+    data = {"foo": "bar"}
     details = Details(data)
-    assert details.foo == data['foo']
+    assert details.foo == data["foo"]
 
 
 def test_getattr_fail():
-    data = {'foo': 'bar'}
+    data = {"foo": "bar"}
     details = Details(data)
     with pytest.raises(Exception):
         details.blah
 
 
 def test_all():
-    data = {'foo': 'bar', 'ham': 'eggs'}
+    data = {"foo": "bar", "ham": "eggs"}
     details = Details(data)
     assert details.all == data
