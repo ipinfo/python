4e346ea6cd04a0562b9d54759a5fdd3a80fc6ece
Uman Shahzad <uman@mslm.io>
2020-12-16 11:12:05 +0500

    comments for util funcs

--- ipinfo/handler_utils.py
+++ ipinfo/handler_utils.py
@@ -24,12 +24,23 @@ def get_headers(access_token):
     return headers
 
 def format_details(details, countries):
+    """
+    Format details given a countries object.
+
+    The countries object can be retrieved from read_country_names.
+    """
     details["country_name"] = countries.get(details.get("country"))
     details["latitude"], details["longitude"] = read_coords(
         details.get("loc")
     )
 
 def read_coords(location):
+    """
+    Given a location of the form `<lat>,<lon>`, returns the latitude and
+    longitude as a tuple.
+
+    Returns None for each tuple item if the form is invalid.
+    """
     lat, lon = None, None
     coords = tuple(location.split(",")) if location else ""
     if len(coords) == 2 and coords[0] and coords[1]:
